4da946db4304d510a2c6eda5b21e95ffbe49b2a7
Firegame224 <xg474627@gmail.com>
2026-05-02 12:50:46 +0700

    feat(user): menambahkan userRepository dan Service method

---
+++ app/src/main/java/com/fiky/lofo_app/screens/item/user/ItemUserScreen.kt
@@ -0,0 +1,12 @@
+package com.fiky.lofo_app.screens.item.user
+
+import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
+
+@Composable
+fun ItemUserScreen(
+    onBack: () -> Unit,
+    viewModel: ItemUserViewModel = viewModel()
+) {
+    val state = viewModel
+}
